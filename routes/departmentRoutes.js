489bc998d8b4115cb9326af0207633b57a4c5ec4
KatyKedi <katykat5bros@gmail.com>
2022-03-23 10:16:04 -0700

    get and post routes for all endpoints, put route for employee

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -19,4 +19,23 @@ router.get('/departments', (req, res) => {
     });
 });
 
+router.post('/departments', ({ body }, res) => {
+    const sql = 
+    `INSERT INTO department (dp_name)
+    VALUES (?)
+    `;
+    const params = [body.dp_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 module.exports = router;
